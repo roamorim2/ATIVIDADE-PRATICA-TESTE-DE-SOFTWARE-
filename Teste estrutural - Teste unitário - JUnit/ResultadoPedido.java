6ff074d44426a798da5f618043c4ac88be9eda67
Rômulo Amorim <romuloead2005@gmail.com>
2026-09-19 17:51:14 -0300

    Delete Teste estrutural - Teste unitário - JUnit/ResultadoPedido.java

--- Teste estrutural - Teste unitário - JUnit/ResultadoPedido.java
+++
@@ -1,4 +0,0 @@
-package br.edu.ifpr.pedidos;
-
-public record ResultadoPedido(String status, long subtotalCentavos,
-                              long descontoCentavos, long freteCentavos, long totalCentavos) { }
