f515e8892f404327eb2fe1ccc9a7f392dc1d92b5
Rômulo Amorim <romuloead2005@gmail.com>
2026-09-19 17:51:03 -0300

    Delete Teste estrutural - Teste unitário - JUnit/ProcessadorPagamento.java

--- Teste estrutural - Teste unitário - JUnit/ProcessadorPagamento.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpr.pedidos;
-
-@FunctionalInterface
-public interface ProcessadorPagamento {
-    /** Retorna true para aprovação, false para recusa definitiva.
-     * IllegalStateException representa indisponibilidade temporária. */
-    boolean autorizar(long totalCentavos);
-}
