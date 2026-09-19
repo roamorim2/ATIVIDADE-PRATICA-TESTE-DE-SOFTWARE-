cddeb09f9328b90f63a465cade8d0b4d08e86808
Rômulo Amorim <romuloead2005@gmail.com>
2026-09-19 17:49:38 -0300

    Delete Teste estrutural - Teste unitário - JUnit/Cliente.java

--- Teste estrutural - Teste unitário - JUnit/Cliente.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpr.pedidos;
-
-public record Cliente(boolean vip, boolean bloqueado, int comprasAnteriores) {
-    public Cliente {
-        if (comprasAnteriores < 0) throw new IllegalArgumentException("Histórico inválido");
-    }
-}
